f6003cf06fec86db641e08966a8a0a26a292561a
Adam Grygierek <adam.grygierek@gmail.com>
2016-12-14 22:35:02 +0100

    Improvements in detection function, calculation and comparing Ruffier index

--- main.py
+++ main.py
@@ -33,21 +33,39 @@ def image_extract(image_file):
     """Function that extract number from image"""
     image = Image.open(image_file)
     width, height = image.size
-    left = 0
+# parameters for crop (best fit)
+    left = 300
     top = 200
-    right = width  # / 2
+    right = width - 400  # / 2
     bottom = height - 700  # / 2
     image = image.crop((left, top, right, bottom))
+# extracting string from image
     value = (pytesseract.image_to_string(image))
-    value = int(value[:-7])
-    return value
+# extracting numbers (intigers) from string
+    i_value = int(filter(str.isdigit, value))
+    return i_value
+
 
 def ruffier_index(squats_result):
-    index = 0
+    index = (sum(squats_result) - 200) / 10
     return index
+
+
+def ruffier_compare(index):
+    if index >= 0:
+        return "bardzo dobry"
+    elif index <= 5:
+        return "dobra"
+    elif index <= 10:
+        return "srednia"
+    else:
+        return "suaba"
+
 
 loaded_image_list = image_read()
 sorted_dictionary = image_sort(loaded_image_list)
-print sorted_dictionary
-first_image = image_extract(sorted_dictionary[0])
-print first_image
+extracted_its = []
+for item in sorted_dictionary:
+    extracted_its.append(image_extract(item))
+compared = ruffier_compare(ruffier_index(extracted_its))
+print compared
